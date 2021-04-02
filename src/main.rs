4d76e7684436bcb30cb1b0c513be00d861508738
PickledChair <ubatamamoon@gmail.com>
2021-04-03 07:24:38 +0900

    REPL実行時にユーザー名を表示するように変更

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use rusty_monkey::repl::start;
 
+use whoami;
 use std::io;
 
 fn main() {
-    println!("Hello! This is the Monkey programming language!");
+    println!("Hello {}! This is the Monkey programming language!", whoami::username());
     println!("Feel free to type in commands");
     if start(&mut io::stdin().lock(), &mut io::stdout()).is_err() {
         println!("Couldn't start the Monkey interpreter.");
